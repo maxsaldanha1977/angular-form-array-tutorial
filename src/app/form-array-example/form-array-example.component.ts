9a7b4521e4d906124aeb9ff47ddc7e98c19cbac5
Max Saldanha <maxuelsaldanha@gmail.com>
2023-12-01 07:46:12 -0300

    nii

---
+++ src/app/form-array-example/form-array-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-array-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-array-example.component.html',
+  styleUrl: './form-array-example.component.css'
+})
+export class FormArrayExampleComponent {
+
+}
